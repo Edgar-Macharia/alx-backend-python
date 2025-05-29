4edf5e789a215e3ff4286a8b12028504ba5893f5
Edgar Macharia <m.edgarmacharia@gmail.com>
2025-05-29 16:49:12 +0300

    add tests for Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from fixtures import TEST_PAYLOAD
@@ -24,3 +24,13 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, test_payload)
+    
+    def test_public_repos_url(self) -> None:
+        """Test that GithubOrgClient._public_repos_url returns the expected repos URL."""
+        test_payload = {"repos_url": "https://api.github.com/orgs/testorg/repos"}
+        with patch.object(GithubOrgClient, 'org',  new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = test_payload
+            client = GithubOrgClient("testorg")
+            result = client._public_repos_url
+            self.assertEqual(result, test_payload["repos_url"])
+            
